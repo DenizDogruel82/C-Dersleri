889170a7f304f184c0110a1282aa023ad32f9416
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-07-30 15:16:31 +0300

    Ders 1 Eklendi

---
+++ C#ders1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ders1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Merhaba ");
+            //Console.Write("Dunya");
+
+            Console.Write("Adinizi Giriniz:");
+            Console.Write("Ali");
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleYazıYazdırma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleYazıYazdırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba");
+            Console.WriteLine("Ali");
+            Console.Write("Adinizi Giriniz: ");
+            Console.WriteLine("Ali");
+       
+            Console.ReadLine();
+        }
+    }
+}
