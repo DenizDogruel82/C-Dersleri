dc8c42d08aff69808702abe214410105f938d901
Deniz <yazilimdeniz082@gmail.com>
2026-08-01 11:07:18 +0300

    değişkenler
    değişkenler eklendi

---
+++ Değişkenler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Değişkenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int yas = 25;
+            //string ad = "deniz";
+            //double ondaliklisayi = 3.14;
+            //bool satistami = true;
+
+            //Console.WriteLine(yas);
+            //Console.WriteLine(ad);
+            //Console.WriteLine(ondaliklisayi);
+            //Console.WriteLine(satistami);
+            //int dogumyili = 2005;
+            //int simdiki = DateTime.Now.Year;
+            //int yas = simdiki - dogumyili;
+            //Console.WriteLine($"yasiniz:{yas}");
+
+            //string ad = "Deniz";
+            //string soyad = "doğruel";
+            //Console.WriteLine($"Adiniz:{ad}");
+            //Console.WriteLine($"Soyadiniz:{soyad}");
+
+            //int sinav1 = 70;
+            //int sinav2 = 80;
+            //int sinav3 = 90;
+            //double ortalama=(sinav1+ sinav2+sinav3)/3;
+            //Console.WriteLine($"Ortalamanız:{ortalama}");
+
+
+
+            Console.Read();
+        }
+    }
+}
