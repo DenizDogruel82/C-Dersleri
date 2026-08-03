74a9c931d4bb4cda1f268af973b8aeb22a76b4f2
Deniz <yazilimdeniz082@gmail.com>
2026-08-03 12:18:00 +0300

    klavyeden veri alma
    klavyeden veri alma komutları eklendi

---
+++ KlavyedenVerialmakomutlari/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlavyedenVerialmakomutlari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("isminizi Giriniz: ");
+            //string ad = Console.ReadLine();
+            //Console.Write("Soyadinizi Giriniz: ");
+            //string soyad = Console.ReadLine();
+            //Console.WriteLine($"Adiniz:{ad} Soyadiniz:{soyad}");
+            Console.WriteLine("kullanici adinizi giriniz: ");
+            string kullaniciadi= Console.ReadLine();
+            Console.WriteLine("sifre giriniz: ");
+                string sifre= Console.ReadLine();
+            Console.WriteLine(sifre);
+            Console.WriteLine(kullaniciadi);
+
+
+            Console.Read();
+
+
+        }
+    }
+}
